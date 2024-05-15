0c70fcde4a78d53ff4cc053439f880d01950fba3
VageeshaDeSilva <suvadesilva@gmail.com>
2024-05-16 00:38:11 +0530

    deleteStudent deleteMapping changed

--- src/main/java/edu/vageesha/controller/StudentController.java
+++ src/main/java/edu/vageesha/controller/StudentController.java
@@ -39,7 +39,7 @@ public class StudentController {
         return ResponseEntity.ok(studentService.updateStudent(id,student));
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<String> removeStudent(@PathVariable Long id){
         return studentService.removeStudent(id)?
